a9867d774ef88da7e264e78eb23b8a0bd08cd839
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-05-12 21:07:08 +0530

    Added support for config file

--- src/BladeUniconsServiceProvider.php
+++ src/BladeUniconsServiceProvider.php
@@ -6,25 +6,38 @@ namespace Codeat3\BladeUnicons;
 
 use BladeUI\Icons\Factory;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Container\Container;
 
 final class BladeUniconsServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->callAfterResolving(Factory::class, function (Factory $factory) {
-            $factory->add('unicons', [
-                'path' => __DIR__.'/../resources/svg',
-                'prefix' => 'uni',
-            ]);
+        $this->registerConfig();
+
+        $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
+            $config = $container->make('config')->get('blade-unicons', []);
+
+            $factory->add('unicons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
         });
+
+    }
+
+    private function registerConfig(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/blade-unicons.php', 'blade-unicons');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-uni'),
-            ], 'blade-uni');
+                __DIR__.'/../resources/svg' => public_path('vendor/blade-unicons'),
+            ], 'blade-unicons');
+
+            $this->publishes([
+                __DIR__.'/../config/blade-unicons.php' => $this->app->configPath('blade-unicons.php'),
+            ], 'blade-unicons-config');
         }
     }
+
 }
