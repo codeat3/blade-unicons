d0998084d08e607f21504b35e570581e5d34cba0
swapnilsarwe <swapnilsarwe@users.noreply.github.com>
2022-07-26 05:25:41 +0000

    Fix styling

--- src/BladeUniconsServiceProvider.php
+++ src/BladeUniconsServiceProvider.php
@@ -17,24 +17,24 @@ final class BladeUniconsServiceProvider extends ServiceProvider
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('blade-unicons', []);
 
-            $factory->add('unicons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+            $factory->add('unicons', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
         });
     }
 
     private function registerConfig(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/blade-unicons.php', 'blade-unicons');
+        $this->mergeConfigFrom(__DIR__ . '/../config/blade-unicons.php', 'blade-unicons');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-unicons'),
+                __DIR__ . '/../resources/svg' => public_path('vendor/blade-unicons'),
             ], 'blade-uni'); // TODO: update this alias to `blade-unicons` in next major release
 
             $this->publishes([
-                __DIR__.'/../config/blade-unicons.php' => $this->app->configPath('blade-unicons.php'),
+                __DIR__ . '/../config/blade-unicons.php' => $this->app->configPath('blade-unicons.php'),
             ], 'blade-unicons-config');
         }
     }
